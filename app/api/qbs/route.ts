698055eaa8c539e443dea2d72ac4a83548600c0a
loiseman <166154345+loiseman@users.noreply.github.com>
2025-08-12 14:42:56 +0200

    Update route.ts

--- app/api/qbs/route.ts
+++ app/api/qbs/route.ts
@@ -1,5 +1,5 @@
 
-import { prisma } from '@/lib/prisma';
+import { prisma } from '../../../lib/prisma';
 export async function GET() {
   const qbs = await prisma.qB.findMany({ where: { active: true }, orderBy: { team: 'asc' } });
   return Response.json(qbs);
